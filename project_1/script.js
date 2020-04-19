71931ecb4856115f6adec0c275083279247b84ff
serhiibabanov <serhii.babanov@gmail.com>
2020-04-18 21:25:51 -0400

    finished task

---
+++ project_1/script.js
@@ -0,0 +1,17 @@
+'use strict';
+let money;
+let time;
+money = prompt("Your budget for a month?","");
+time = prompt("Enter date in format YYYY-MM-DD","");
+let appData = {
+    budget : money,
+    timeData : time,
+    expenses : {},
+    optionalExpenses : {},
+    income : [],
+    saving : false
+};
+let expenses = prompt("Enter a required cost item this month","");
+let expensesMoney = prompt("How much will it cost?","");
+appData.expenses.expenses = expensesMoney;
+alert("Your budget for a 1 day - " + appData.budget / 30);
