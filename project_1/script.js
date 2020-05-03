4bf71a9bba3dff5502c6fbe8aaa569cc789c6395
serhiibabanov <serhii.babanov@gmail.com>
2020-05-02 20:41:47 -0400

    second exercise

--- project_1/script.js
+++ project_1/script.js
@@ -1,7 +1,7 @@
 'use strict';
 let money;
 let time;
-money = prompt("Your budget for a month?","");
+money = +prompt("Your budget for a month?","");
 time = prompt("Enter date in format YYYY-MM-DD","");
 let appData = {
     budget : money,
@@ -11,7 +11,51 @@ let appData = {
     income : [],
     saving : false
 };
-let expenses = prompt("Enter a required cost item this month","");
-let expensesMoney = prompt("How much will it cost?","");
-appData.expenses.expenses = expensesMoney;
-console.log("Your budget for a 1 day - " + appData.budget / 30);
+/* for (let i = 0; i < 2; i++){
+    let a = prompt("Enter a required cost item this month","");
+    let b = +prompt("How much will it cost?","");
+    if (typeof(a) === 'string' && typeof(a) != null && typeof(b) != null && a != '' && b != '' ){
+        console.log("done");
+        appData.expenses[a] = b;
+    } else {
+        i--;
+    }
+} */
+
+/* let i = 0;
+while ( i < 2) {
+    let a = prompt("Enter a required cost item this month","");
+    let b = +prompt("How much will it cost?","");
+    if (typeof(a) === 'string' && typeof(a) != null && typeof(b) != null && a != '' && b != '' ){
+        console.log("done");
+        appData.expenses[a] = b;
+    } else {
+        i--;
+    }
+    i++;
+} */
+
+let i = 0;
+ do {
+    let a = prompt("Enter a required cost item this month","");
+    let b = +prompt("How much will it cost?","");
+    if (typeof(a) === 'string' && typeof(a) != null && typeof(b) != null && a != '' && b != '' ){
+        console.log("done");
+        appData.expenses[a] = b;
+    } else {
+        i--;
+    }
+    i++;
+} while ( i < 2);
+
+appData.MoneyPerDay = appData.budget / 30;
+if (appData.MoneyPerDay < 100){
+    console.log("Low");
+} else if (appData.MoneyPerDay > 100 && appData.MoneyPerDay <3000){
+    console.log("Medium");
+} else if (appData.MoneyPerDay > 3000){
+    console.log("High");
+} else {
+    console.log("Error");
+}
+console.log("Your budget for a 1 day - " + appData.MoneyPerDay);
